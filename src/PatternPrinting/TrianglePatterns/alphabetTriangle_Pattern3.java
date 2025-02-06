0d1e21cdfb2517d365109485cbf5d2b6c85f9121
itsme-swe <me.harshmehra07@gmail.com>
2025-02-06 15:59:12 +0530

    Update

---
+++ src/PatternPrinting/TrianglePatterns/alphabetTriangle_Pattern3.java
@@ -0,0 +1,25 @@
+package PatternPrinting.TrianglePatterns;
+
+public class alphabetTriangle_Pattern3 {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print((char)(j+64) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+A
+A B
+A B C
+A B C D
+
+* */
