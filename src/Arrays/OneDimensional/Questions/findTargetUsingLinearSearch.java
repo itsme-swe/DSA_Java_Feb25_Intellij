7fbfc2cf00e21b3d140acc9bf52b26d4bb4a482d
itsme-swe <me.harshmehra07@gmail.com>
2025-02-09 12:23:11 +0530

    Update

---
+++ src/Arrays/OneDimensional/Questions/findTargetUsingLinearSearch.java
@@ -0,0 +1,39 @@
+package Arrays.OneDimensional.Questions;
+
+public class findTargetUsingLinearSearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 1, 10, 12, 8, 7};
+
+        int target = -1;
+
+        findTarget(arr,target);
+
+        // Target not in array
+    }
+
+    public static void findTarget(int[] arr, int t) {
+
+        int n = arr.length;
+
+        boolean flag = false;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == t) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println("Target found");
+        }
+        else {
+            System.out.println("Target not in array");
+        }
+
+    }
+
+}
+
