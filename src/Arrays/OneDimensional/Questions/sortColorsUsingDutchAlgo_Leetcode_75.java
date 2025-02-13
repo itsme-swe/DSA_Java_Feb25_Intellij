e2ad0167dd553bd8f7d2010f5a3e9871f5030e3b
itsme-swe <me.harshmehra07@gmail.com>
2025-02-13 21:13:46 +0530

    Update

---
+++ src/Arrays/OneDimensional/Questions/sortColorsUsingDutchAlgo_Leetcode_75.java
@@ -0,0 +1,27 @@
+/*
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Leetcode - https://leetcode.com/problems/sort-colors/description/
+
+*/
+
+package Arrays.OneDimensional.Questions;
+
+public class sortColorsUsingDutchAlgo_Leetcode_75 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,0,2,1,1,0};
+
+
+    }
+
+    public static void sortColors(int[] nums) {
+
+    }
+}
