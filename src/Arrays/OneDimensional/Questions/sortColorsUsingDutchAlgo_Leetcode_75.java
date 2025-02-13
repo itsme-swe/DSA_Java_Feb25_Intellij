126f371b67aec1705898df8da66256a6ac15aa71
itsme-swe <me.harshmehra07@gmail.com>
2025-02-13 21:21:56 +0530

    Update

--- src/Arrays/OneDimensional/Questions/sortColorsUsingDutchAlgo_Leetcode_75.java
+++ src/Arrays/OneDimensional/Questions/sortColorsUsingDutchAlgo_Leetcode_75.java
@@ -18,10 +18,45 @@ public class sortColorsUsingDutchAlgo_Leetcode_75 {
 
         int[] arr = {2,0,2,1,1,0};
 
+        sortColors(arr);
+
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+
 
     }
 
     public static void sortColors(int[] nums) {
 
+        int n = nums.length;
+
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while (mid < high) {
+
+            if(nums[mid] == 0) {
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums,high,mid);
+                high--;
+            }
+        }
+
+    }
+
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
+
+// 0 0 1 1 2 2
