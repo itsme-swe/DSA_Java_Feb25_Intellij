d577323cc93dbc1050dd0ec566139750ea006312
itsme-swe <me.harshmehra07@gmail.com>
2025-02-23 05:43:56 +0530

    Update

---
+++ src/Arrays/TwoDimensional/Questions/pascalTriangle_Leetcode118.java
@@ -0,0 +1,25 @@
+/*
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it, as shown:
+
+       1
+      1  1
+     1  2  1
+    1  3  3  1
+   1  4  6  4  1
+
+Leetcode - https://leetcode.com/problems/pascals-triangle/description/
+
+*/
+
+package Arrays.TwoDimensional.Questions;
+
+public class pascalTriangle_Leetcode118 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
